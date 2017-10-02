8d5703b66b1c688e377110577ab18365f2b79560
Masaki <masaki.kawabata@gmail.com>
2017-10-02 17:43:23 -0300

    ok

--- html/network/index.php
+++ html/network/index.php
@@ -2,11 +2,11 @@
 $data = [];
 if (($handle = fopen("/var/tmp/fing.network", "r")) !== FALSE) {
     while (($data[] = fgetcsv($handle, 1000, ",")) !== FALSE) {
-	//$num = count($data);
+        //$num = count($data);
     }
     fclose($handle);
 }
 
 //$csv = file_get_contents('/var/tmp/fing.network');
-echo json_encode($data));
+echo json_encode($data);
 ?>
