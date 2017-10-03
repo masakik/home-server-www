80d234068d047e8950b88a041bdcbe3f813d53eb
Masaki <masaki.kawabata@gmail.com>
2017-10-03 11:55:31 -0300

    k

--- html/network/index.php
+++ html/network/index.php
@@ -1,12 +1,40 @@
 <?php
+if (isset($_GET['f']) and $_GET['f'] == 'up') {
+
+}
+
 $data = [];
+$labels = ['datetime', 'state', 'ip', 'field', 'host', 'mac', 'vendor'];
+
+$x = simplexml_load_file('/var/tmp/log.xml');
+//print_r($x);
+echo json_encode($x);exit;
+
+
+
+$j = file_get_contents('/var/tmp/network.json');
+$json = str_replace("'",'"',$j);
+$json = str_replace(",}",'}',$json);
+$json = str_replace(",]",']',$json);
+
+echo $json;exit;
+
 if (($handle = fopen("/var/tmp/fing.network", "r")) !== FALSE) {
-    while (($data[] = fgetcsv($handle, 1000, ";")) !== FALSE) {
-        //$num = count($data);
+    $count = 0;
+    while (($tmp = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        if ($tmp[5])
+        $data[$count] = [];
+        for ($i = 0; $i < 7; $i++) {
+            $label = $labels[$i];
+            $data[$count][$label] = $tmp[$i];
+            //print_r($data);
+        }
+        $count++;
     }
     fclose($handle);
 }
 
 //$csv = file_get_contents('/var/tmp/fing.network');
+print_r($data);exit;
 echo json_encode($data);
 ?>
