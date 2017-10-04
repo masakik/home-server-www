16f603a361eae9ee953dba145de4c8e1bb98dc2f
Masaki <masaki.kawabata@gmail.com>
2017-10-04 10:43:56 -0300

    minor change

--- html/network/index.php
+++ html/network/index.php
@@ -2,7 +2,6 @@
 
 $x = simplexml_load_file('/var/tmp/fing.network');
 $x->mtime = date("d-m-Y H:i:s", filemtime('/var/tmp/fing.network'));
-//print_r($x);
-echo json_encode($x);
 
+echo json_encode($x);
 ?>
--- html/system/index.php
+++ html/system/index.php
@@ -7,37 +7,8 @@ $data['OS'] = exec('uname -a', $ret);
 $data['DiskUsage'] = exec('df / --output=used -h|grep -v "Used" ', $ret);
 //$data['DiskTotal'] = disk_total_space('/');
 $data['DiskTotal'] = exec('df / --output=size -h|grep -v "Used" ', $ret);
+$data['ip'] = $_SERVER['SERVER_ADDR'];
 
-
-echo json_encode($data);exit;
-
-
-unset($ret);
-
-$uname = $ret[0];
-
+echo json_encode($data);
 ?>
-Temperatura da CPU: <?= $t1 ?><br/>
-Sistema: <?= $uname ?> <br/>
 
-<pre>
-<?php
-
-echo shell_exec('whoami');
-//echo "pi\n";
-//echo shell_exec('ls /home/pi -la');
-//echo "tmp\n";
-//echo shell_exec('ls /var/tmp/ -la');
-
-
-if (file_exists('/var/tmp/network.json')) {
-    $network = file_get_contents('/var/tmp/network.json');
-    echo $network;
-    print_r(json_decode($network));
-    echo json_last_error();
-} else {
-    echo "nao existe";
-}
-?>
-</pre>
-OK
