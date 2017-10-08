66daf009a5f5bcbb9d1b8a55b89efc4c707402fc
Masaki RASP1B <masaki.kawabata@gmail.com>
2017-10-07 21:06:52 -0300

    ok

--- html/system/index.php
+++ html/system/index.php
@@ -5,10 +5,18 @@ $data['CpuTemp'] = round($ret[0] / 1000, 1);
 $data['OS'] = exec('uname -a', $ret);
 
 $data['DiskUsage'] = exec('df / --output=used -h|grep -v "Used" ', $ret);
-//$data['DiskTotal'] = disk_total_space('/');
 $data['DiskTotal'] = exec('df / --output=size -h|grep -v "Used" ', $ret);
+
 $data['ip'] = $_SERVER['SERVER_ADDR'];
 
+$file = fopen('/var/tmp/system.updates','r');
+$updt['datetime'] = trim(fgets($file,50));
+$updt['data'] = array();
+while (($line = fgets($file,50)) !== false) {
+  $updt['data'][] = trim($line);
+}
+fclose($file);
+$data['updates'] = $updt;
 echo json_encode($data);
 ?>
 
